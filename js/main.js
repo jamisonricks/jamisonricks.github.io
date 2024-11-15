48e25094aa9f6d22a4126bbcb0bacfd9c871ff43
Jamison Ricks <jamison.ricks2@gmail.com>
2024-11-15 01:04:57 -0500

    added id to p

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 let start_btn = document.getElementById('game-button-1');
 
 start_btn.addEventListener('click', function startGame() {
-	let text_box = document.getElementById('game-text-box')
+	let text_box = document.getElementById('game-text')
 
-	text_box.children[0].innerHTML = 'I have changed!';
+	text_box.innerHTML = 'I have changed!';
 });
 
